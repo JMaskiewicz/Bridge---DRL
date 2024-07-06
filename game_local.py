8dabf789ffaa6e24c7c2c9b8ed97b2c260bd0e51
jmask <j.maskiewicz@student.uw.edu.pl>
2024-07-06 13:11:19 +0200

    bidding part of game making additional rules from bridge

--- game_local.py
+++ game_local.py
@@ -1,20 +1,11 @@
 import random
 
-
-'''
-quarterback - one player that won bidding and will play the hand
-partner - the player sitting opposite the quarterback
-defenders 1 - the other who will play against the quarterback and have Whist lead
-defenders 2 - the other who will play against the quarterback and have second lead
-'''
-
 # Constants
 SUITS = ['C', 'D', 'H', 'S']  # Clubs, Diamonds, Hearts, Spades
 RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 BID_SUITS = ['C', 'D', 'H', 'S', 'NT']  # Club, Diamond, Heart, Spade, No Trump
+
 VALID_BIDS = [f"{rank}{suit}" for rank in range(1, 8) for suit in BID_SUITS]
-
-
 
 class Card:
     def __init__(self, suit, rank):
@@ -24,7 +15,6 @@ class Card:
     def __repr__(self):
         return f"{self.rank}{self.suit}"
 
-
 class Deck:
     def __init__(self):
         self.cards = [Card(suit, rank) for suit in SUITS for rank in RANKS]
@@ -32,7 +22,6 @@ class Deck:
 
     def deal(self):
         return self.cards.pop()
-
 
 class Player:
     def __init__(self, name):
@@ -48,7 +37,6 @@ class Player:
 
     def show_hand(self):
         return ', '.join(map(str, self.hand))
-
 
 class Bidding:
     def __init__(self):
@@ -93,7 +81,6 @@ class Bidding:
     def print_bidding_history(self):
         return '\n'.join([f"{player}: {bid}" for player, bid in self.bids])
 
-
 class Trick:
     def __init__(self):
         self.cards = []
@@ -101,15 +88,16 @@ class Trick:
     def play_card(self, player, card):
         self.cards.append((player, card))
 
-    def determine_winner(self):
-        # Assuming the first card determines the suit to follow
+    def determine_winner(self, trump_suit):
         leading_suit = self.cards[0][1].suit
         winning_card = self.cards[0]
         for player, card in self.cards:
-            if card.suit == leading_suit and RANKS.index(card.rank) > RANKS.index(winning_card[1].rank):
+            if card.suit == trump_suit:
+                if winning_card[1].suit != trump_suit or RANKS.index(card.rank) > RANKS.index(winning_card[1].rank):
+                    winning_card = (player, card)
+            elif card.suit == leading_suit and (winning_card[1].suit != trump_suit) and RANKS.index(card.rank) > RANKS.index(winning_card[1].rank):
                 winning_card = (player, card)
         return winning_card[0]
-
 
 class GameLogic:
     def __init__(self):
@@ -118,6 +106,9 @@ class GameLogic:
         self.bidding = Bidding()
         self.current_player_index = 0
         self.tricks = []
+        self.trump_suit = None
+        self.declarer = None
+        self.tricks_won = {1: 0, 2: 0}  # Tracks tricks won by each team
 
     def start_game(self):
         self.deal_cards()
@@ -147,6 +138,12 @@ class GameLogic:
         return True
 
     def play_card(self, player, card):
+        # Check if the player follows the lead suit if possible
+        if self.tricks and self.tricks[-1].cards:
+            leading_suit = self.tricks[-1].cards[0][1].suit
+            if any(c.suit == leading_suit for c in player.hand) and card.suit != leading_suit:
+                print(f"You must follow suit with {leading_suit} if you have one.")
+                return False
         for p_card in player.hand:
             if str(p_card) == card:
                 self.tricks[-1].play_card(player, player.play_card(p_card))
@@ -154,12 +151,17 @@ class GameLogic:
         print("Invalid card. Try again.")
         return False
 
+    def set_trump_and_declarer(self):
+        if self.bidding.current_highest_bid:
+            bid_suit = self.bidding.current_highest_bid[-1]
+            self.trump_suit = None if bid_suit == 'NT' else bid_suit
+            self.declarer = self.bidding.declarer
+
     def set_current_player_to_next_of_declarer(self):
         for i, player in enumerate(self.players):
             if player == self.bidding.declarer:
                 self.current_player_index = (i + 1) % 4
                 break
-
 
 def main():
     game = GameLogic()
@@ -187,6 +189,7 @@ def main():
     print("Bidding history:\n", game.bidding.print_bidding_history())
 
     if game.bidding.declarer:
+        game.set_trump_and_declarer()
         game.set_current_player_to_next_of_declarer()
 
         # Playing phase
@@ -201,13 +204,33 @@ def main():
                 if game.play_card(current_player, card):
                     game.next_turn()
 
-            trick_winner = game.tricks[-1].determine_winner()
+            trick_winner = game.tricks[-1].determine_winner(game.trump_suit)
             print(f"{trick_winner.name} wins the trick!")
+
+            if trick_winner == game.players[0] or trick_winner == game.players[2]:
+                game.tricks_won[1] += 1
+            else:
+                game.tricks_won[2] += 1
 
         print("Game over!")
         for player in game.players:
-            tricks_won = sum(1 for trick in game.tricks if trick.determine_winner() == player)
+            tricks_won = sum(1 for trick in game.tricks if trick.determine_winner(game.trump_suit) == player)
             print(f"{player.name} won {tricks_won} tricks.")
+
+        if game.bidding.declarer in [game.players[0], game.players[2]]:
+            contract = int(game.bidding.current_highest_bid[0])
+            print(f"Declarer's team needed to win {contract + 6} tricks.")
+            if game.tricks_won[1] >= contract + 6:
+                print("Declarer's team fulfilled the contract!")
+            else:
+                print("Declarer's team failed to fulfill the contract.")
+        else:
+            contract = int(game.bidding.current_highest_bid[0])
+            print(f"Declarer's team needed to win {contract + 6} tricks.")
+            if game.tricks_won[2] >= contract + 6:
+                print("Declarer's team fulfilled the contract!")
+            else:
+                print("Declarer's team failed to fulfill the contract.")
     else:
         print("No valid contract was made. The game ends with no contract.")
 
